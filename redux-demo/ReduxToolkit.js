d802c053eb4ab86da91c79cdcefbe82d096a894b
kunaldhongade <kunaldhongade1@gmail.com>
2023-12-14 23:28:03 +0530

    Redux demo complete

---
+++ redux-demo/ReduxToolkit.js
@@ -0,0 +1,23 @@
+// Problems with redux
+
+/* 
+
+Redux require too much boiler
+
+- Action
+- Action Object
+- Action creator
+- Switch statement in a reducer
+
+A lot of other packages have to be installed to work with redux
+
+- Redux-thunk
+- immer
+- Redux-devtools
+
+Redux toolkit is the official, opinionated, batteries-included toolset for efficient redux development
+
+- Abstract over the setup process
+- handle the most common use cases
+- include some useful utilities
+*/
